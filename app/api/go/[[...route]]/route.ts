cf6f29efb7364e6a940561227bac4066360a8ebb
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2025-04-22 21:58:47 -0300

    ix: Debug geolocation

--- app/api/go/[[...route]]/route.ts
+++ app/api/go/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { redirectRoutes } from "../routes/redirect.routes";
 import { GeoMiddleware } from "hono-geo-middleware";
+import { getGeo } from "../helpers/getGeo";
 
 export const runtime = "nodejs";
 
@@ -11,6 +12,15 @@ const app = new Hono().basePath("/api/go");
 app.route("/links", linkRoutes);
 app.route("/redirect", redirectRoutes);
 app.use("/*", GeoMiddleware());
+app.get("/geo", (c) => {
+    const country = c.req.header("x-vercel-ip-country");
+    const region = c.req.header("x-vercel-ip-country-region");
+    const city = c.req.header("x-vercel-ip-city");
+
+    const geo = getGeo(c);
+
+    return c.json({ country, region, city, geo });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
